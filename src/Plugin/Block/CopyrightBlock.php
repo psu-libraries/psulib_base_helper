36b8b2c433c448a86b14772d0de7ec6f7add33fc
Mike Henninger <msh6004@psu.edu>
2024-08-09 14:21:20 -0400

    Issue #1 - Adding copyright block and template.

---
+++ src/Plugin/Block/CopyrightBlock.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\psulib_base_helper\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a copyright block block.
+ *
+ * @Block(
+ *   id = "psulib_base_helper_copyright_block",
+ *   admin_label = @Translation("Copyright Block"),
+ *   category = @Translation("PSU Libraries"),
+ * )
+ */
+final class CopyrightBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build['content'] = [
+      '#theme' => 'psul_copyright',
+    ];
+    return $build;
+  }
+
+}
